4e1c98c66e44c1a78a42502881630a60d25c5f88
chandan ghosh <chandan493@gmail.com>
2016-07-25 21:42:42 +0530

    Added project

---
+++ src/main/java/com/spr/exception/ShopNotFound.java
@@ -0,0 +1,13 @@
+package com.spr.exception;
+
+public class ShopNotFound extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ShopNotFound(String cause) {
+		// TODO Auto-generated constructor stub
+		super(cause);
+	}
+}
